35da58501ed40c6bea2799b6685168f8ec8acb8d
yubinkarki <iamyubinkarki@gmail.com>
2024-07-01 08:50:02 +0545

    Delete file

--- SessionOne/MyClass/RunMyClass.cs
+++
@@ -1,9 +0,0 @@
-namespace ExploringTheNet.SessionOne.MyClass;
-
-public static class RunAnimalClass {
-    public static void RunAnimal() {
-        Animal dog = new(name: "Golden", age: 0, species: "Nice");
-        dog.AnimalDetails(); 
-        dog.MakeSound();
-    }
-}
