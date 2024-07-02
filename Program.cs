4a4898ebe9866335a0a4b000d9d67bb0b1b8ce68
yubinkarki <iamyubinkarki@gmail.com>
2024-07-02 11:50:46 +0545

    Add check for arg length

--- Program.cs
+++ Program.cs
@@ -1,7 +1,21 @@
-﻿namespace ExploringTheNet;
+﻿using System;
 
-class Program {
+using ExploringTheNet.Constant;
+using ExploringTheNet.SessionOne.AppBasic;
+using ExploringTheNet.SessionOne.AppClass;
+using ExploringTheNet.SessionOne.AppInterface;
+
+namespace ExploringTheNet;
+
+internal static class Program {
     private static void Main(string[] args) {
-        Console.WriteLine("Hello, World!");
+        if (args.Length > 0) {
+            string combinedArgs = string.Join(", ", args);
+            Console.WriteLine("Here are the args: " + combinedArgs);
+        } else {
+            const string message = AppString.RunWithoutArgument;
+            Console.WriteLine($"{message}\n");
+            RunAppBasic.RunBaseType();
+        }
     }
 }
