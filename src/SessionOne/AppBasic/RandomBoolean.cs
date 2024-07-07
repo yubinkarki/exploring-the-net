2ff0592a0a84593b2bf080b2163d0f3f8f2493b5
yubinkarki <iamyubinkarki@gmail.com>
2024-07-07 11:03:52 +0545

    Boolean type and random method

---
+++ src/SessionOne/AppBasic/RandomBoolean.cs
@@ -0,0 +1,22 @@
+namespace ExploringTheNet.SessionOne.AppBasic;
+
+using System;
+
+public static class RandomBoolean {
+    public static void Run() {
+        bool isChecked = false;
+        int randomNumber = new Random().Next(0, 100);
+
+        bool isLess = randomNumber < 50;
+
+        Console.WriteLine($"Random number: {randomNumber}");
+
+        if (isChecked) {
+            Console.WriteLine("This is inside isChecked");
+        }
+
+        if (isLess) {
+            Console.WriteLine($"Random number is less");
+        }
+    }
+}
