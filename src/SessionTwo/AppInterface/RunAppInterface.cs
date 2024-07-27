edc236eb7ec2e2157630d8fdb5c3400d6c4cb305
yubinkarki <iamyubinkarki@gmail.com>
2024-07-27 10:11:52 +0545

    Rename file

--- src/SessionTwo/AppInterface/RunAppInterface.cs
+++ src/SessionTwo/AppInterface/RunAppInterface.cs
@@ -1,7 +1,7 @@
 namespace ExploringTheNet.SessionTwo.AppInterface;
 
 public class RunAppInterface {
-    public static void RunAppShape() {
+    public static void RunShape() {
         Circle ball = new();
         ball.Radius = 10.0;
         ball.Name = "Football";
