44d305bb467f3783405777d3cd02d223673078b9
yubinkarki <iamyubinkarki@gmail.com>
2024-09-30 19:58:01 +0545

    Add enum

---
+++ src/Constant/AppEnum.cs
@@ -0,0 +1,11 @@
+namespace ExploringTheNet.Constant;
+
+public static class AppEnum {
+    public enum Month {
+        jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec
+    }
+
+    public enum UserRole {
+        admin, hr, user, agent
+    }
+}
