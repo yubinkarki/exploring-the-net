e2b3f040c2eb8a07757ff20f7ee550d149c5b9e3
yubinkarki <iamyubinkarki@gmail.com>
2025-03-16 21:45:05 +0545

    Create another class

--- src/SessionOne/AppBasic/ValuePoint.cs
+++ src/SessionOne/AppBasic/ValuePoint.cs
@@ -1,6 +1,27 @@
 namespace ExploringTheNet.src.SessionOne.AppBasic;
 
+using System;
+
 public class ValuePoint {
     public int X { get; set; }
     public int Y { get; set; }
+}
+
+public class CopyByValue {
+    public static void Run() {
+        ValuePoint pointOne = new ValuePoint();
+        pointOne.X = 10;
+
+        ValuePoint pointTwo = pointOne; // Assigning a copy/reference of pointOne to pointTwo
+
+        Console.WriteLine("Before changing value of x in pointOne:");
+        Console.WriteLine($"Point one X >> {pointOne.X}");
+        Console.WriteLine($"Point two X >> {pointTwo.X}");
+
+        pointOne.X = 20; // Changing value of X in pointOne
+
+        Console.WriteLine("\nAfter changing value of x in pointOne:");
+        Console.WriteLine($"Point one X >> {pointOne.X}");
+        Console.WriteLine($"Point two X >> {pointTwo.X}");
+    }
 }
