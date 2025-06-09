0e551e25ea55bb7b54035a2d11bc114e54cfec3a
yubinkarki <iamyubinkarki@gmail.com>
2025-06-09 20:30:32 +0545

    Add new address model

---
+++ src/SessionFour/AppLinq/AddressModel.cs
@@ -0,0 +1,15 @@
+namespace ExploringTheNet.src.SessionFour.AppLinq;
+
+public class AddressModel {
+    public int Id { get; set; }
+    public string City { get; set; }
+    public int EmployeeId { get; set; }
+    public string StreetName { get; set; }
+
+    public AddressModel(int id, string city, int employeeId, string streetName) {
+        this.Id = id;
+        this.City = city;
+        this.EmployeeId = employeeId;
+        this.StreetName = streetName;
+    }
+}
