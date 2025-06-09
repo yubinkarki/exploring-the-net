7540562fe6db3ace080bdd4065bea542dedff1ab
yubinkarki <iamyubinkarki@gmail.com>
2025-06-09 20:30:25 +0545

    Rename file

--- src/SessionFour/AppLinq/EmployeeModel.cs
+++ src/SessionFour/AppLinq/EmployeeModel.cs
@@ -1,15 +1,13 @@
 namespace ExploringTheNet.src.SessionFour.AppLinq;
 
-public class Employee {
+public class EmployeeModel {
     public int Id { get; set; }
     public string Name { get; set; }
-    public string Address { get; set; }
     public decimal Salary { get; set; }
 
-    public Employee(int id, string name, string address, decimal salary) {
+    public EmployeeModel(int id, string name, decimal salary) {
         this.Id = id;
         this.Name = name;
-        this.Address = address;
         this.Salary = salary;
     }
 }
